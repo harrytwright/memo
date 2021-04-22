3f2c3d4d4c333c0099a305569e943d0034fb2216
harrytwright <haroldtomwright@gmail.com>
2021-04-22 13:53:11 +0100

    src: link up
    Signed-off-by: harrytwright <haroldtomwright@gmail.com>

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -3,9 +3,12 @@
  * */
 
 import React from 'react'
+import Memo from "../Memo";
 
 export default function App () {
   return (
-    <div style={{ width: '720px', marginLeft: 'auto', marginRight: 'auto' }} />
+    <div style={{ width: '720px', marginLeft: 'auto', marginRight: 'auto' }}>
+      <Memo style={{ margin: 'auto', marginTop: '64px' }} />
+    </div>
   )
 }
