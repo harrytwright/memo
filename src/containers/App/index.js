0491028d038ced0ba2a3a84d402b2e5422add864
harrytwright <haroldtomwright@gmail.com>
2021-04-22 13:51:45 +0100

    src: Update to boilerplate
    Signed-off-by: harrytwright <haroldtomwright@gmail.com>

---
+++ src/containers/App/index.js
@@ -0,0 +1,11 @@
+/**
+ * Main page for the app
+ * */
+
+import React from 'react'
+
+export default function App () {
+  return (
+    <div style={{ width: '720px', marginLeft: 'auto', marginRight: 'auto' }} />
+  )
+}
--- src/index.js
+++ src/index.js
@@ -1,21 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { store } from './app/store';
-import { Provider } from 'react-redux';
-import * as serviceWorker from './serviceWorker';
+/**
+ *
+ * index.js
+ *
+ * Entry point of the application
+ */
+
+import 'sanitize.css/sanitize.css'
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { ConnectedRouter } from 'connected-react-router'
+import history from './utils/history'
+
+import reportWebVitals from './reportWebVitals'
+
+// Import root app
+import App from './containers/App'
+
+import configureStore from './configureStore'
+
+// Create redux store with history
+const initialState = {}
+const store = configureStore(initialState, history)
+
+const MOUNT_NODE = document.getElementById('root') || document.createElement('div')
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <ConnectedRouter history={history}>
+        <App />
+      </ConnectedRouter>
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  MOUNT_NODE
 );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+reportWebVitals(console.log)
