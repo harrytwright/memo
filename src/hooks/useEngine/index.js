089d15c7c3a3acb6ae91d4d64ad79a6821ecf8fd
harrytwright <haroldtomwright@gmail.com>
2021-04-26 21:41:58 +0100

    src: Added useEngine hook
    Not 100% on this, would prefer to call it 'createEngine' but holding to the react naming conventions for now
    
    Signed-off-by: harrytwright <haroldtomwright@gmail.com>

---
+++ src/hooks/useEngine/index.js
@@ -0,0 +1,59 @@
+/**
+ * Stockfish UCI parser hook
+ *
+ * This should be generic in-case we switch what engine is being used
+ * */
+
+import { useCallback, useMemo } from 'react'
+
+/**
+ * Message handler from the engine
+ *
+ * @callback StockfishMessageHandler
+ * @param {string} line - The UCI response
+ * */
+
+/**
+ * The UCI command for the engine
+ *
+ * @typedef {String} UCICommand
+ * */
+
+/**
+ *
+ * */
+const useEngine = () => {
+  const engine = useMemo(() => window.STOCKFISH(), [])
+
+  /**
+   * Send a UCI command to the engine
+   *
+   * @param {UCICommand} command
+   * */
+  const send = useCallback((command) => {
+    engine.postMessage(command)
+  }, [engine])
+
+  /**
+   * Handle the response from the engine
+   *
+   * @param {StockfishMessageHandler} callback
+   * */
+  const handleMessage = useCallback((callback) => {
+    engine.onmessage = callback
+  }, [engine])
+
+  /**
+   * A fake engine object to just change the way Stockfish response is handled
+   * */
+  const ngin = {
+    /**
+     * @param {Function} callback
+     * */
+    onMessage: handleMessage
+  }
+
+  return [ngin, send]
+}
+
+export default useEngine
