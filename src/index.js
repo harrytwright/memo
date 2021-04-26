16a3c849147ec3a4b016f1697be93c21df109dd8
harrytwright <haroldtomwright@gmail.com>
2021-04-26 21:42:54 +0100

    lint: small lint
    Signed-off-by: harrytwright <haroldtomwright@gmail.com>

--- src/index.js
+++ src/index.js
@@ -35,7 +35,7 @@ ReactDOM.render(
     </Provider>
   </React.StrictMode>,
   MOUNT_NODE
-);
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
