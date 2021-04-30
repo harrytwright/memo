404a6c6ea511fe35f4a4048c3ec8d7ad5c572307
harrytwright <haroldtomwright@gmail.com>
2021-04-30 19:20:29 +0100

    src: documentation added
    Signed-off-by: harrytwright <haroldtomwright@gmail.com>

--- src/components/History/index.js
+++ src/components/History/index.js
@@ -11,9 +11,13 @@ import PropTypes from 'prop-types'
 import Wrapper from './Wrapper'
 import { TableView, Index, Move } from './components'
 
+/**
+ *
+ * */
 const History = ({ history, selected, onSelect = () => {} }) => {
   if (!selected) selected = history.length
 
+  // Since this comes in as one giant array just split it here
   const chunkedArray = history.reduce((all, one, i) => {
     const ch = Math.floor(i / 2)
     all[ch] = [].concat((all[ch] || []), one)
@@ -24,11 +28,14 @@ const History = ({ history, selected, onSelect = () => {} }) => {
     <Wrapper>
       <TableView>
         {chunkedArray.map((chunk, idx) => {
+          // We need to deal with half moves later, so get the original index here
           const mv = (idx * 2)
           return (
             <Fragment key={idx}>
               <Index>{idx + 1}</Index>
               {chunk.map((el, idx) => {
+                // Make the ply here by adding the index on + 1 so the ply != there index of
+                // history (May change this based on how select works)
                 const ply = mv + (idx + 1)
                 return <Move className={clsx((ply === selected) && 'active')} key={ply} onClick={onSelect}>{el.san}</Move>
               })}
